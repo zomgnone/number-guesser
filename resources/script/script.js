1b82f15cce9809efb03c5279034fe9cd31336799
Andrew S <44890623+zomgnone@users.noreply.github.com>
2022-03-27 12:34:32 +0300

    Initial commit

---
+++ resources/script/script.js
@@ -0,0 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+// This function will be called at the start of each new round 
+// in order to generate the new secret target number.
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+// This function will be called each round to determine 
+// which guess is closest to the target number.
+
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+  const humanDistance = Math.abs(humanGuess - targetNumber);
+  const computerDistance = Math.abs(computerGuess - targetNumber);
+  // Return true if human wins; return false if computer wins.
+  return humanDistance < computerDistance ? true : false;
+}
+
+// This function will be used to correctly increase 
+// the winner’s score after each round.
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    humanScore++;
+  }
+  if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+// This function will be used to update the round number after each round.
+const advanceRound = () => currentRoundNumber++;
